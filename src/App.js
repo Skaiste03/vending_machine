ded4cda9506ca472595793bf535db5d551317256
Skaiste <smacaityte@gmail.com>
2023-05-19 11:22:02 +0300

    Updated

--- src/App.js
+++ src/App.js
@@ -4,18 +4,17 @@ function App() {
   return (
     <div>
       <div className='wrapper'>
-        <div>
-          <div className='description'>
-            <h1>Vending machine</h1>
-            <div>
-              <p>Your snack is just three steps away</p>
-              <p>1. Choose a snack and note its ID number.</p>
-              <p>2. Insert the coins.</p>
-              <p>3. Click 'Order'.</p>
-              <p>Enjoy your snack and don't forget collect your change!</p>
-            </div>
+        <div className='description'>
+          <h1>Vending machine</h1>
+          <div>
+            <p>Your snack is just three steps away</p>
+            <p>1. Choose a snack and note its ID number.</p>
+            <p>2. Insert the coins.</p>
+            <p>3. Click 'Order'.</p>
+            <p>Enjoy your snack and don't forget collect your change!</p>
           </div>
         </div>
+
         <VendingMachine />
       </div>
     </div>
